fecdeb43a1736e7e665b0d0b6cf4d32feb3c0520
SetiNgabo45 <mapito.ngabo@ocsbstudent.ca>
2021-01-13 15:51:36 +0000

    js:12:4 js:12:4 js:12:4 js:12:4 js:12:4 js:12:4 js:12:4 js:12:4 js:12:4

--- script.js
+++ script.js
@@ -8,41 +8,41 @@ function pizza () {
   if ((size === 'Large') && (top === 1)) {
     total = (6 + 1) * 1.13
     total = total.toFixed(2)
-    alert('The total cost is $' + total) 
-    } 
+    alert('The total cost is $' + total)
+  } 
   else if ((size === 'Extra large') && (top === 1)) {
     total = (10 + 1) * 1.13
     total = total.toFixed(2)
     alert('The total cost is $' + total)
-    }
+  }
   if ((size === 'Large') && (top === 2)) {
     total = ( 6 + 1.75 ) * 1.13
     total = total.toFixed(2)
     alert('The total cost is $' + total)
-    }
+  }
   else if ((size === 'Extra large') && (top === 2)) {
     total = (10 + 1.75) * 1.13
     total = total.toFixed(2)
     alert('The total cost is $' + total)
-    }
+  }
   if ((size === 'Large') && (top === 3)) {
     total = (6 + 2.50) * 1.13
     total = total.toFixed(2)
     alert('The total cost is $' + total)
-    }
+  }
   else if ((size === 'Extra large') && (top === 3)) {
     total = (10 + 2.50) * 1.13
     total = total.toFixed(2)
     alert('The total cost is $' + total)
-    }
+  }
   if ((size === 'Large') && (top === 4)) {
     total = (6 + 3.35) * 1.13
     total = total.toFixed(2)
     alert('The total cost is $' + total)
-    }
+  }
   else if ((size === 'Extra large') && (top === 4)) {
     total = (10 + 3.35) * 1.13
     total = total.toFixed(2)
     alert('The total cost is $' + total)
-    }
+  }
 }
